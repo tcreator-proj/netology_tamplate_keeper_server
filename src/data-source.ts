0a3a09f948e991a46cb8ad5eddddd8e9df3b87c9
TomuraCreator <42077765+TomuraCreator@users.noreply.github.com>
2022-04-19 15:18:32 +0300

    data-source is connected

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,16 +1,17 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { Expert } from "./entity/Expert"
+import 'dotenv/config'
 
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: process.env.DB_HOST,
     port: 5432,
     username: process.env.DB_USERNAME,
-    password: process.env.PASS,
+    password: process.env.DB_PASS,
     database: process.env.DB_DBNAME,
     synchronize: true,
-    logging: true,
+    logging: false,
     entities: [Expert],
     migrations: [],
     subscribers: [],
--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,17 @@ import { Expert } from "./entity/Expert"
 AppDataSource.initialize().then(async () => {
 
     console.log("Inserting a new user into the database...")
-    const user = new Expert()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+    const expert = new Expert()
+    expert.email = "expert1@gmail.com"
+    expert.expert_id = 1
+    expert.firstName = "Verdandy"
+    expert.surname = "Expertof"
+    await AppDataSource.manager.save(expert)
+    console.log("Saved a new user with id: " + expert.email)
 
     console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
+    const experts = await AppDataSource.manager.find(Expert)
+    console.log("Loaded users: ", experts)
 
     console.log("Here you can setup and run express / fastify / any other framework.")
 
